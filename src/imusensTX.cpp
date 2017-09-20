b28935e30d38fc0fd8f903fba091be730f5ab2c8
Matthias Yzusqui <matthias.yzusqui@ipg-automotive.com>
2017-09-20 10:04:48 +0200

    imusens_tx: rename imu to marg

--- src/imusensTX.cpp
+++ src/imusensTX.cpp
@@ -40,11 +40,11 @@ volatile static float dt = 0, ts = 0;
 #endif
 
 // Globals
-mpu9250 vhclImu(10, MOSI_PIN_28);       // MPU9250 1 On SPI bus 0 at csPin 10
+mpu9250 vhclMarg(10, MOSI_PIN_28);       // MPU9250 1 On SPI bus 0 at csPin 10
 #ifndef SPI_ONLY
-mpu9250 headImu(0x69, 2, I2C_PINS_3_4); // MPU9250 2 On I2C bus 2 at addr 0x69
+mpu9250 headMarg(0x69, 2, I2C_PINS_3_4); // MPU9250 2 On I2C bus 2 at addr 0x69
 #else
-mpu9250 headImu(17, MOSI_PIN_21);       // MPU9250 2 On SPI bus 1 at csPin 17
+mpu9250 headMarg(17, MOSI_PIN_21);       // MPU9250 2 On SPI bus 1 at csPin 17
 #endif /* SPI_ONLY */
 
 FusionFilter vhclFilter, headFilter;
@@ -55,11 +55,11 @@ MetroExt task_usbTx  = MetroExt(1000);      //   1 msec
 MetroExt task_dbgOut = MetroExt(2000000);   //   2 sec
 MetroExt task_dbgIRS = MetroExt(2000000);   //   2 sec
 
-static float imuData1[10], imuData2[10];
+static float margData1[10], margData2[10];
 
 void irs1Func_vhcl()
 {
-    vhclImu.GetAllData(imuData1, HS_TRUE);
+    vhclMarg.GetAllData(margData1, HS_TRUE);
 #ifndef I2C_SPI_TIME
     if (task_dbgIRS.check()) {
         digitalWrite(ledPin, !digitalRead(ledPin));
@@ -74,9 +74,9 @@ void irs2Func_head()
 #endif /* I2C_SPI_TIME */
 
 #ifndef SPI_ONLY
-    headImu.RequestAllData();
+    headMarg.RequestAllData();
 #else
-    headImu.GetAllData(imuData2, HS_TRUE);
+    headMarg.GetAllData(margData2, HS_TRUE);
 #endif /* SPI_ONLY */
 
 #ifdef I2C_SPI_TIME
@@ -117,97 +117,97 @@ void setup()
 
     /* IMU 1 (Vehicle) Setup */
     // Setup bus specifics for this device
-    vhclImu.WireSetup();
+    vhclMarg.WireSetup();
 
     // Start the bus (only one device needs to start it on a shared bus)
-    vhclImu.WireBegin();
+    vhclMarg.WireBegin();
 
     // Skip setup if this MPU9250 is not online
-    if (vhclImu.whoAmI() != 0x71) goto next;
+    if (vhclMarg.whoAmI() != 0x71) goto next;
 
     if (Debug) Serial.printf("MPU9250 (1): 9-axis motion sensor is online\n");
 
     // Start by performing self test (and reporting values)
-    vhclImu.SelfTest();
+    vhclMarg.SelfTest();
     delay(1000);
 
     if (Debug) Serial.printf("MPU9250 (1): Calibrating gyro and accel\n");
 
     // Calibrate gyro and accelerometers, load biases into bias registers
-    vhclImu.AcelGyroCal();
+    vhclMarg.AcelGyroCal();
 
     if (Debug) Serial.printf("MPU9250 (1): Initialising for active data mode ...\n");
 
     // Config for normal operation, set sample-rate div to '0x0X + 1'
-    vhclImu.Init(ACCEL_RANGE_2G, GYRO_RANGE_500DPS, 0x00);
+    vhclMarg.Init(ACCEL_RANGE_2G, GYRO_RANGE_500DPS, 0x00);
 
     // Check if AK8963 magnetometer is online
-    if (Debug) Serial.printf("AK8963  (1): I'm 0x%02x\n", vhclImu.whoAmIAK8963());
+    if (Debug) Serial.printf("AK8963  (1): I'm 0x%02x\n", vhclMarg.whoAmIAK8963());
 
     // Get magnetometer calibration from AK8963 ROM
-    vhclImu.InitAK8963(MAG_RANGE_16BIT, MAG_RATE_100HZ);
+    vhclMarg.InitAK8963(MAG_RANGE_16BIT, MAG_RATE_100HZ);
 
 #ifdef RESET_MAGCAL
     // TODO: Add RawHid msg to wave device and also notice on done
     Serial.printf("AK8963  (1) initialized for active data mode ...\n");
     Serial.printf("Mag Calibration: Wave device in a figure eight until done!\n");
     delay(4000);
-    vhclImu.MagCal();
+    vhclMarg.MagCal();
 #else
     if (Debug) Serial.printf("AK8963  (1): Mag calibration using pre-recorded values\n");
-    vhclImu.SetMagCal(magHardIron, magSoftIron);
+    vhclMarg.SetMagCal(magHardIron, magSoftIron);
 #endif /* RESET_MAGCAL */
 
 next:
     /* IMU 2 (Head) Setup */
     // Setup bus specifics for this device
-    headImu.WireSetup();
+    headMarg.WireSetup();
 
     // Start the bus (only one device needs to start it on a shared bus)
-    headImu.WireBegin();
+    headMarg.WireBegin();
 
     // Skip setup if this MPU9250 is not online
-    if (headImu.whoAmI() != 0x71) goto end;
+    if (headMarg.whoAmI() != 0x71) goto end;
 
     if (Debug) Serial.printf("MPU9250 (2): 9-axis motion sensor is online\n");
 
     // Start by performing self test (and reporting values)
-    headImu.SelfTest();
+    headMarg.SelfTest();
     delay(1000);
 
     if (Debug) Serial.printf("MPU9250 (2): Calibrating gyro and accel\n");
 
     // Calibrate gyro and accelerometers, load biases into bias registers
-    headImu.AcelGyroCal();
+    headMarg.AcelGyroCal();
 
     if (Debug) Serial.printf("MPU9250 (2): Initialising for active data mode...\n");
 
     // Config for normal operation, set sample-rate div to '0x0X + 1'
-    headImu.Init(ACCEL_RANGE_2G, GYRO_RANGE_500DPS, 0x00);
+    headMarg.Init(ACCEL_RANGE_2G, GYRO_RANGE_500DPS, 0x00);
 
     // Check if AK8963 magnetometer is online
-    if (Debug) Serial.printf("AK8963  (2): I'm 0x%02x\n", headImu.whoAmIAK8963());
+    if (Debug) Serial.printf("AK8963  (2): I'm 0x%02x\n", headMarg.whoAmIAK8963());
 
     // Get magnetometer calibration from AK8963 ROM
-    headImu.InitAK8963(MAG_RANGE_16BIT, MAG_RATE_100HZ);
+    headMarg.InitAK8963(MAG_RANGE_16BIT, MAG_RATE_100HZ);
 
 #ifdef RESET_MAGCAL
     // TODO: Add RawHid msg to wave device and also notice on done
     Serial.printf("AK8963  (2) initialized for active data mode...\n");
     Serial.printf("Mag Calibration: Wave device in a figure eight until done!\n");
     delay(4000);
-    headImu.MagCal();
+    headMarg.MagCal();
 #else
     if (Debug) Serial.printf("AK8963  (2): Mag calibration using pre-recorded values\n");
-    headImu.SetMagCal(magHardIron, magSoftIron);
+    headMarg.SetMagCal(magHardIron, magSoftIron);
 #endif /* RESET_MAGCAL */
 
 end:
     if (Debug) Serial.printf("\nSetup done, enabling interrupts!\n");
 
     // Enable interrupts
-    vhclImu.EnableInterrupt(intPin1_vhcl, irs1Func_vhcl);
-    headImu.EnableInterrupt(intPin2_head, irs2Func_head);
+    vhclMarg.EnableInterrupt(intPin1_vhcl, irs1Func_vhcl);
+    headMarg.EnableInterrupt(intPin2_head, irs2Func_head);
     interrupts();
 
     // Wait for the host application to be ready
@@ -227,8 +227,8 @@ void loop()
 
 #ifndef SPI_ONLY
     // Read the i2c rx buffer when done
-    if (headImu.RequestedAvailable()) {
-        headImu.GetAllData(imuData2, HS_FALSE);
+    if (headMarg.RequestedAvailable()) {
+        headMarg.GetAllData(margData2, HS_FALSE);
     }
 #endif /* SPI_ONLY */
 
@@ -237,13 +237,13 @@ void loop()
     if (task_filter.check()) {
 
         noInterrupts();
-        vhclFilter.MadgwickUpdate(imuData1[0], imuData1[1], imuData1[2],
-                                  imuData1[4], imuData1[5], imuData1[6],
-                                  imuData1[7], imuData1[8], imuData1[9], chrono_1.Split());
+        vhclFilter.MadgwickUpdate(margData1[0], margData1[1], margData1[2],
+                                  margData1[4], margData1[5], margData1[6],
+                                  margData1[7], margData1[8], margData1[9], chrono_1.Split());
 
-        headFilter.MadgwickUpdate(imuData2[0], imuData2[1], imuData2[2],
-                                  imuData2[4], imuData2[5], imuData2[6],
-                                  imuData2[7], imuData2[8], imuData2[9], chrono_2.Split());
+        headFilter.MadgwickUpdate(margData2[0], margData2[1], margData2[2],
+                                  margData2[4], margData2[5], margData2[6],
+                                  margData2[7], margData2[8], margData2[9], chrono_2.Split());
         interrupts();
 
         // Get quat rotation difference, store result in tx_buffer[0:3]
@@ -301,15 +301,15 @@ void loop()
 
         if (Debug == 2) {
             // The current raw sensor data
-            Serial.printf("%6.3f\t%6.3f\t%6.3f\n",   imuData1[0], imuData1[1], imuData1[2]);
-            Serial.printf("%6.2f\n",                 imuData1[3]);
-            Serial.printf("%6.3f\t%6.3f\t%6.3f\n",   imuData1[4], imuData1[5], imuData1[6]);
-            Serial.printf("%6.2f\t%6.2f\t%6.2f\n\n", imuData1[7], imuData1[8], imuData1[9]);
+            Serial.printf("%6.3f\t%6.3f\t%6.3f\n",   margData1[0], margData1[1], margData1[2]);
+            Serial.printf("%6.2f\n",                 margData1[3]);
+            Serial.printf("%6.3f\t%6.3f\t%6.3f\n",   margData1[4], margData1[5], margData1[6]);
+            Serial.printf("%6.2f\t%6.2f\t%6.2f\n\n", margData1[7], margData1[8], margData1[9]);
 
-            Serial.printf("%6.3f\t%6.3f\t%6.3f\n",   imuData2[0], imuData2[1], imuData2[2]);
-            Serial.printf("%6.2f\n",                 imuData2[3]);
-            Serial.printf("%6.3f\t%6.3f\t%6.3f\n",   imuData2[4], imuData2[5], imuData2[6]);
-            Serial.printf("%6.2f\t%6.2f\t%6.2f\n\n", imuData2[7], imuData2[8], imuData2[9]);
+            Serial.printf("%6.3f\t%6.3f\t%6.3f\n",   margData2[0], margData2[1], margData2[2]);
+            Serial.printf("%6.2f\n",                 margData2[3]);
+            Serial.printf("%6.3f\t%6.3f\t%6.3f\n",   margData2[4], margData2[5], margData2[6]);
+            Serial.printf("%6.2f\t%6.2f\t%6.2f\n\n", margData2[7], margData2[8], margData2[9]);
         }
 
         // Reset print counters
